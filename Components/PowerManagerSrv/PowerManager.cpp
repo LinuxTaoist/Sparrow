404b641f48688f3829a05eaa3f5ad8f130c5b020
LinuxTaoist <dx_65535@163.com>
2023-12-21 21:19:43 +0800

    调整文件名

--- Components/PowerManagerSrv/PowerManager.cpp
+++ Components/PowerManagerSrv/PowerManager.cpp
@@ -2,27 +2,17 @@
  *------------------------------------------------------------------------------
  *  @copyright Copyright (c) 2022  <dx_65535@163.com>.
  *
- *  @file       : LogManager.h
+ *  @file       : PowerManager.cpp
  *  @author     : Xiang.D (dx_65535@163.com)
  *  @version    : 1.0
  *  @brief      : Blog: https://linuxtaoist.gitee.io
- *  @date       : 2023/11/25
+ *  @date       : 2023/12/21
  *
  *
  *  Change History:
  *  <Date>     | <Version> | <Author>       | <Description>
  *------------------------------------------------------------------------------
- *  2023/11/25 | 1.0.0.1   | Xiang.D        | Create file
+ *  2023/12/21 | 1.0.0.1   | Xiang.D        | Create file
  *------------------------------------------------------------------------------
  *
  */
-#include "SprObserver.h"
-
-class LogManager : public SprObserver
-{
-public:
-    LogManager(ModuleIDType id, const std::string& name, std::shared_ptr<SprMediatorProxy> mMsgMediatorPtr);
-    virtual ~LogManager();
-
-    int ProcessMsg(const SprMsg& msg);
-};
