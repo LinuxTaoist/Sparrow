1632e781a3fe818745fbc64538f00a4b758fc67e
LinuxTaoist <dx_65535@163.com>
2024-06-19 18:50:29 +0800

    【详细描述】xtools增加指令 【修改类型】Optimize 【问题链接】N/A 【项目关联】Default

--- Tools/xtools.sh
+++ Tools/xtools.sh
@@ -1,12 +1,63 @@
 #!/bin/bash
 
 # ANSI escape codes for colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NC='\033[0m' # No Color
+BLACK='\033[0;30m'        # Black
+RED='\033[0;31m'          # Red
+GREEN='\033[0;32m'        # Green
+BROWN='\033[0;33m'        # Brown (or Yellow on some terminals)
+YELLOW='\033[1;33m'       # Bold Yellow
+BLUE='\033[0;34m'         # Blue
+PURPLE='\033[0;35m'       # Purple (or Magenta)
+CYAN='\033[0;36m'         # Cyan
+WHITE='\033[0;37m'        # White
+LGREY='\033[0;37m'        # Light Grey (same as white on many terminals)
+DGREY='\033[1;30m'        # Dark Grey
+LBLUE='\033[1;34m'        # Light Blue
+LPURPLE='\033[1;35m'      # Light Purple (or Light Magenta)
+LCYAN='\033[1;36m'        # Light Cyan
+LRED='\033[1;31m'         # Light Red
+LGREEN='\033[1;32m'       # Light Green
+LYELLOW='\033[1;33m'      # Light Yellow
+LBROWN='\033[0;93m'       # Light Brown (or Gold)
+DBLUE='\033[0;34m'        # Dark Blue
 
-# Function to build the project
+# No Color
+NC='\033[0m'
+
+# Capture start time
+start_time=$(date +%s)
+
+# Delimiter
+delimiter="================================================================================"
+
+# Function to display completion details
+display_completion_details() {
+    end_time=$(date +%s)
+    duration=$((end_time - start_time))
+
+    echo ""
+    echo -e "${GREEN}================================================================================${NC}"
+    echo -e "${GREEN}所有操作已完成。${NC}"
+    echo -e "${GREEN}完成时间：$(date '+%Y-%m-%d %H:%M:%S') 耗时 ${duration} 秒。${NC}"
+    echo ""
+}
+
+# cmd env
+show_env() {
+    # 显示当前环境变量
+    echo -e "${PURPLE}==========================    Display  Environment    ==========================${NC}"
+    # 显示Ubuntu版本
+    echo -e "${PURPLE}- Ubuntu version: $(lsb_release -ds) ${NC}"
+    # 显示g++版本
+    echo -e "${PURPLE}- g++ version:    $(g++ --version | head -n1) ${NC}"
+    # 显示gcc版本
+    echo -e "${PURPLE}- gcc version:    $(gcc --version | head -n1) ${NC}"
+    # 显示cmake版本
+    echo -e "${PURPLE}- cmake version:  $(cmake --version | head -n1) ${NC}"
+    echo -e "${PURPLE}${delimiter}${NC}"
+}
+
+# cmd build-all
 build_project() {
     echo -e "${GREEN}开始编译项目...${NC}"
     make clean && make
@@ -17,7 +68,19 @@ build_project() {
     fi
 }
 
-# Function to perform static code analysis
+## cmd build_3rd
+build_3rdparty() {
+    tools_path=$(pwd)
+    echo -e "${PURPLE}= 开始编译libgo.... ${NC}"
+    cd ${tools_path}/../3rdParty/libgo/
+    ./build.sh
+
+    echo -e "${PURPLE}= 开始编译3rdParty.... ${NC}"
+    cd ${tools_path}/../3rdParty/sqlite/
+    ./build.sh
+}
+
+# cmd static_scan
 static_scan() {
     echo -e "${GREEN}开始静态代码扫描...${NC}"
     cppcheck --enable=all --inconclusive src/
@@ -31,53 +94,52 @@ static_scan() {
 # Function to print usage information with logo
 usage() {
     local logo="\
-                                                                        +++
-     +++++      +++++ ++++++++++++++++                                  +++
-      ++++      ++++  ++++++++++++++++                                  +++
-      +++++    +++++  ++++++++++++++++                                  +++
-       ++++    ++++         ++++                                        +++
-       +++++  ++++          ++++                                        +++
-        ++++ +++++          ++++           ++++++          ++++++       +++      ++++++
-        ++++ ++++           ++++         ++++++++++      ++++++++++     +++    ++++++++
-         +++++++            ++++        ++++++++++++    ++++++++++++    +++   +++++++++
-         +++++++            ++++        ++++    ++++    ++++    ++++    +++   ++++    +
-          +++++             ++++       +++++     ++++  +++++     ++++   +++   ++++
-         +++++++            ++++       ++++      ++++  ++++      ++++   +++   +++++++
-         ++++++++           ++++       ++++      ++++  ++++      ++++   +++    ++++++++
-        ++++ ++++           ++++       ++++      ++++  ++++      ++++   +++     ++++++++
-       +++++ +++++          ++++       ++++      ++++  ++++      ++++   +++       ++++++
-       ++++   ++++          ++++       ++++      ++++  ++++      ++++   +++         ++++
-      +++++   +++++         ++++        ++++    ++++    ++++    ++++    +++   +     ++++
-      ++++     +++++        ++++        ++++++++++++    ++++++++++++    +++   ++++++++++
-     +++++     +++++        ++++         ++++++++++      ++++++++++     +++   +++++++++
-    +++++       +++++       ++++           ++++++          ++++++       +++    ++++++
- "
-
-    echo "========================================================================================="
-    echo -e "${GREEN}${logo}${NC}"
-    echo "========================================================================================="
-    echo ""
-    echo "DevTools - Linux C++ 中间件开发辅助工具"
-    echo ""
-    echo "Usage:"
-    echo "  $0 <command>"
-    echo ""
-    echo "Commands:"
-    echo "  build       编译整个项目"
-    echo "  staticscan  执行静态代码扫描"
-    echo "  help        显示此帮助信息"
+                XTools - Sparrow 开发辅助工具
+                                                     +++
+     +++    +++  +++++++++++                         +++
+     ++++  ++++  +++++++++++                         +++
+      +++  +++       +++                             +++
+       ++++++        +++        +++++       +++++    +++    +++++
+       ++++++        +++      ++++++++    ++++++++   +++  +++++++
+        ++++         +++      +++++++++   +++++++++  +++  +++   +
+        ++++         +++     ++++   +++  ++++   +++  +++  +++
+       ++++++        +++     +++    +++  +++    +++  +++  ++++++
+       ++++++        +++     +++    +++  +++    +++  +++    +++++
+      +++  +++       +++     ++++  ++++  ++++  ++++  +++       +++
+     ++++  ++++      +++      ++++++++    ++++++++   +++  +    +++
+     +++    +++      +++      ++++++++    ++++++++   +++  +++++++
+    ++++    ++++     +++        ++++        ++++     +++  +++++
+    "
+    echo -e "${LPURPLE}================================================================================${NC}"
+    echo -e "${LPURPLE}${logo}${NC}"
+    echo -e "${LPURPLE}================================================================================${NC}"
+    echo -e ""
+    echo -e "${PURPLE}Usage:${NC}"
+    echo -e "${PURPLE}  $0 env              查看当前环境${NC}"
+    echo -e "${PURPLE}  $0 build-all        编译整个项目${NC}"
+    echo -e "${PURPLE}  $0 build-3rd        编译依赖的第三方库${NC}"
+    echo -e "${PURPLE}  $0 staticscan       执行静态代码扫描${NC}"
+    echo -e "${PURPLE}  $0 help             显示此帮助信息${NC}"
+    echo -e ""
+    echo -e "${PURPLE}================================================================================${NC}"
 }
 
 # Main function to handle command-line arguments and dispatch commands
 main() {
     if [ $# -eq 0 ]; then
+        clear
         usage
         exit 1
     fi
 
     case "$1" in
-        build)
-            build_project
+        env)
+            show_env
+            ;;
+        build-all)
+            ;;
+        build-3rd)
+            build_3rdparty
             ;;
         staticscan)
             static_scan
@@ -86,10 +148,13 @@ main() {
             usage
             ;;
         *)
-            echo -e "${RED}未知命令: $1${NC}"
-            usage
+            echo -e "${RED}未知命令: $1${NC} "
+            echo -e "${RED}请使用 \"$0 help\" 命令查看帮助信息。${NC}"
+            exit 1
             ;;
     esac
+
+    display_completion_details
 }
 
 # Call the main function with all passed arguments
