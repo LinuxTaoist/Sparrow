270faff1a44f4387d1d16426716e4ad15de5b3ce
LinuxTaoist <dx_65535@163.com>
2025-05-28 08:01:03 +0800

    优化CMake编译
    【详细描述】N/A
    【修改类型】Optimize
    【问题链接】N/A
    【项目关联】Default

--- modules_configs.cmake
+++ modules_configs.cmake
@@ -1,12 +1,15 @@
 # modules_configures.cmake
 set(MODULE_CONFIG_VERSION "DEFAULT_MCONFIG_1001")
 
-# 业务模块 Components/Business编译开关
+## 业务模块配置
+# 业务组件，根据需要增加BUSINESS_MODULES中的模块
+# 对应Components/Business路径下的文件夹名
 set(BUSINESS_MODULES "")
 list(APPEND BUSINESS_MODULES OneNetMqtt)
 
-# 其他编译默认值, 可在Build/options中修改
+## 可选功能模块配置
+# 非核心组件，编译时默认禁用
+# 启用方法：Build/options中修改对应编译项
 set(BUILD_DEBUG OFF CACHE BOOL "Build Debug")
 set(BUILD_EXAMPLES OFF CACHE BOOL "Add Examples")
 set(BUILD_TESTCASE OFF CACHE BOOL "Add Test Case")
-
