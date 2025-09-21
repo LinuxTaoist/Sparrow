b2a6f42b3002de6cc24fece98fd91d28d89b9407
LinuxTaoist <dx_65535@163.com>
2025-09-21 12:10:56 +0000

    update Util/SharedBinaryTree.h. 增加注释
    【详细描述】N/A
    【修改类型】Optimize
    【问题链接】N/A
    【项目关联】Default
    
    Signed-off-by: LinuxTaoist <dx_65535@163.com>

--- Util/SharedBinaryTree.h
+++ Util/SharedBinaryTree.h
@@ -1,3 +1,21 @@
+/**
+ *---------------------------------------------------------------------------------------------------------------------
+ *  @copyright Copyright (c) 2022  <dx_65535@163.com>.
+ *
+ *  @file       : SharedBinaryTree.h
+ *  @author     : Xiang.D (dx_65535@163.com)
+ *  @version    : 1.0
+ *  @brief      : Blog: https://mp.weixin.qq.com/s/eoCPWMGbIcZyxvJ3dMjQXQ
+ *  @date       : 2023/12/13
+ *
+ *
+ *  Change History:
+ *  <Date>     | <Version> | <Author>       | <Description>
+ *---------------------------------------------------------------------------------------------------------------------
+ *  2023/12/23 | 1.0.0.1   | Xiang.D        | Create file
+ *---------------------------------------------------------------------------------------------------------------------
+ *
+ */
 #ifndef __SHARED_BINARY_TREE_H__
 #define __SHARED_BINARY_TREE_H__
 
